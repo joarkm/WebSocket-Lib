56974c49e539480836e4ce6a7effc1d0d95c7c28
Joar K. Mauseth <jkm99412@gmail.com>
2018-04-02 20:33:12 +0200

    Solved some build issues
    MSBuild struggled with having a project within another project's folder.

---
+++ WebSocket-Lib/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebSocketLib
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Main program running");
+        }
+    }
+}
